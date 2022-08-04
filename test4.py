944ba427e474e84b00b3de99ca6323309d54bbe9
ddgavale <dhannanjayg4@gmail.com>
2022-08-04 19:43:51 +0530

    this is my another code for git push

---
+++ test4.py
@@ -0,0 +1,15 @@
+import pymongo
+
+client = pymongo.MongoClient(
+    "mongodb+srv://dhananjayg4:ddg1992@cluster92.t8e4svk.mongodb.net/?retryWrites=true&w=majority"
+)
+db = client.test
+print(db)
+d = {
+    "name": "Dhananjay_4",
+    "surname": "Gavale_4",
+    "Email-id": "dhannanjayg4@gmail.com"
+}
+db1 = client['mongotest4']
+coll = db1['test4']
+coll.insert_one(d)
